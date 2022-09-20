749538001446b941ff654e6e88633b4a80bbfcde
Josiah Henderson <josiahhenderson@C02V5BJDHV27.local>
2022-09-19 17:51:36 -0800

    added goals and saved

---
+++ Users/abehenderson/goals.js
@@ -0,0 +1,16 @@
+const circularDataFlow = (existingCode, newCode) => {
+    let singleSourceOfTruth = existingCode;
+    let localCopy = singleSourceOfTruth;
+    let featureBranch = localCopy + newCode;
+    singleSourceOfTruth += featureBranch;
+    return singleSoureOfTruth;
+  } 
+  // My goals in GitHub:
+    // Learn the basics and build familiarity and confidence in GitHub
+    // Create and develop 2 separate projects with these goals in mind:
+        // Return real value to users with each project (Reading tutoring website; Astrological notification app/website)
+        // Practice and utilize my growing skills and understanding of JavaScript, HTML, CSS, and more
+        // Build a portfolio of work with verifiable work history for potential employers to access
+        // Practice coding in teams to develop real world skills and competencies
+    // Have fun nerding out :-)
+    
