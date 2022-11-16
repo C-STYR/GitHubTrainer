d106a58a23b2f0ac9e3703efb8fea22f3a26665a
Abe Henderson <abehenderson@gmail.com>
2022-11-16 07:29:11 -0900

    Updated with CSX solution

--- Users/abehenderson/CSX/Recursion/repeater.js
+++ Users/abehenderson/CSX/Recursion/repeater.js
@@ -1,18 +1,31 @@
-
-let counter = 0;
+// Attempt #2 (CSX Solution)
 
 function repeater(char) {
-  counter++;  
-  if (counter >= 5) {
-    counter = 0;
-    return char;
-  } else {
-    return char + repeater(char);
-  }
+  if (char.length === 5) return char;
+    return repeater(char + char[0]);
 }
 
-console.log(repeater('g'));
-console.log(repeater('j'));
+console.log(repeater('g')); // logs: 'ggggg'
+console.log(repeater('j')); // logs: 'jjjjj'
 
-// Write a function that takes an input character and returns that character repeated 5 times using recursion. 
-// For example, if the input is 'g', then the output should be 'ggggg'.
+//-------------------------------------------------------------------------------------
+
+// // Attempt #1 (I used a counter in global memory because that's exactly how CSX taught it in the video called 'Recursion: Repeater')
+
+// let counter = 0;
+
+// function repeater(char) {
+//   counter++;  
+//   if (counter >= 5) {
+//     counter = 0;
+//     return char;
+//   } else {
+//     return char + repeater(char);
+//   }
+// }
+
+// console.log(repeater('g'));
+// console.log(repeater('j'));
+
+// // Write a function that takes an input character and returns that character repeated 5 times using recursion. 
+// // For example, if the input is 'g', then the output should be 'ggggg'.
