f06539cc0419d23cbcb9433c6ddac16c1815bc4b
Abe Henderson <abehenderson@gmail.com>
2022-11-17 05:53:03 -0900

    Updated 'groupBy.js' and added an example of destructuring

--- Users/abehenderson/CSX/Callbacks/groupBy.js
+++ Users/abehenderson/CSX/Callbacks/groupBy.js
@@ -1,17 +1,16 @@
 // Attempt #2 
   // Per Cole's Change Request:
     // Initialize a variable equal to invoking the callback on 'el'
-    // Eliminate 'cache' and use '[el]' to temporarily store values
+    // Eliminate 'cache' and use '[el]' to store values
 
 function groupBy(arrays, callback) {
   const obj = {};
   arrays.forEach(el => {
-    let evRes = callback(el); // Cole: what is a better name that 'evRes'?
-    if (!obj[evRes]) {
-      obj[evRes] = [el];
+    let key = callback(el); // Cole: what is a better name that 'key'?
+    if (!obj[key]) {
+      obj[key] = [el];
     } else {
-      [el] = obj[evRes]; // I didn't know I could use '[el]' as a variable like this
-      obj[evRes].push(el); // I didn't know I could use 'obj[evRes].push' like this!
+      obj[key].push(el); // I didn't know I could use 'obj[key].push' like this!
     }
   });
   return obj;
@@ -21,6 +20,15 @@ function groupBy(arrays, callback) {
 const decimals = [1.3, 2.1, 2.4];
 const floored = function(num) { return Math.floor(num); };
 console.log(groupBy(decimals, floored)); // should log: { 1: [1.3], 2: [2.1, 2.4] }
+
+// Destructuring example:
+
+const x = [1, 2, 3, 4, 5, 6, 7];
+
+const [y, ...nums] = x;
+
+console.log(y);
+console.log(...nums);
 
 
 //-------------------------------------------------------------------------------------
