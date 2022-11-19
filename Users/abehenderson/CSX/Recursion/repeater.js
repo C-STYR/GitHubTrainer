841f36d4e043db60d25a3bb140043156dbb13a80
Abe Henderson <abehenderson@gmail.com>
2022-11-19 12:04:11 -0900

    Resolved formatting conflicts

--- Users/abehenderson/CSX/Recursion/repeater.js
+++ Users/abehenderson/CSX/Recursion/repeater.js
@@ -5,27 +5,27 @@ function repeater(char) {
     return repeater(char + char[0]);
 }
 
-console.log(repeater('g')); // logs: 'ggggg'
-console.log(repeater('j')); // logs: 'jjjjj'
+// console.log(repeater('g')); // logs: 'ggggg'
+// console.log(repeater('j')); // logs: 'jjjjj'
 
 //-------------------------------------------------------------------------------------
 
 // // Attempt #1 (I used a counter in global memory because that's exactly how CSX taught it in the video called 'Recursion: Repeater')
 
-// let counter = 0;
+let counter = 0;
 
-// function repeater(char) {
-//   counter++;  
-//   if (counter >= 5) {
-//     counter = 0;
-//     return char;
-//   } else {
-//     return char + repeater(char);
-//   }
-// }
+function repeater(char) {
+  counter++;  
+  if (counter >= 5) {
+    counter = 0;
+    return char;
+  } else {
+    return char + repeater(char);
+  }
+}
 
 // console.log(repeater('g'));
 // console.log(repeater('j'));
 
-// // Write a function that takes an input character and returns that character repeated 5 times using recursion. 
-// // For example, if the input is 'g', then the output should be 'ggggg'.
+// Write a function that takes an input character and returns that character repeated 5 times using recursion. 
+// For example, if the input is 'g', then the output should be 'ggggg'.
