0f533bf6a31ce40ac79483f3654c342ca9caa578
Abe Henderson <abehenderson@gmail.com>
2022-11-23 06:35:23 -0900

    Added solution to 'pluralize' in CSX/Callbacks

---
+++ Users/abehenderson/CSX/Callbacks/pluralize.js
@@ -0,0 +1,15 @@
+function pluralize(array) {
+  array.forEach((string, index) => {
+    array[index] = string.concat('s');
+  })
+  return array;
+}
+
+
+const animals = ["dog", "cat", "tree frog"];
+console.log(pluralize(animals)); // should log: ["dogs", "cats", "tree frogs"]
+
+
+// Create a function pluralize that takes an array of strings as input and returns a new array with an "s" added to the end of each string in the input array. For example, if the string "carrot" is in the input array, it should become the string "carrots" in the output array.
+
+// The body of the pluralize function should employ a single for loop that pluralizes each string in the input array.
