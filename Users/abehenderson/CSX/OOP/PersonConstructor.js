ddd6829257aa2802954d0dc35adb20c7522f6478
Abe Henderson <abehenderson@gmail.com>
2022-11-27 06:46:14 -0900

    Added a solution for 'PersonConstructor' in CSX/OOP

---
+++ Users/abehenderson/CSX/OOP/PersonConstructor.js
@@ -0,0 +1,13 @@
+
+
+function PersonConstructor() {
+	this.greet = function() {console.log('hello')};
+}
+
+const simon = new PersonConstructor();
+
+// Uncomment this line to check your work!
+simon.greet(); // -> Logs 'hello'
+
+// Create a function PersonConstructor that uses the 'this' keyword to save a single property onto its scope called 'greet'. 
+// 'greet' should be a function that logs the string "hello".
