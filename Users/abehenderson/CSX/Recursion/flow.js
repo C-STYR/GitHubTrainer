0b3226915529d17499e3a3b12aedffbce2608876
Abe Henderson <abehenderson@gmail.com>
2022-11-28 06:40:17 -0900

    Added Cole's suggest solution

--- Users/abehenderson/CSX/Recursion/flow.js
+++ Users/abehenderson/CSX/Recursion/flow.js
@@ -1,3 +1,24 @@
+
+// Attempt #3 (Cole's Solution)
+
+function flow(input, funcArray) {
+  if (funcArray.length) {
+    return flow(funcArray.shift()(input), funcArray)
+  }
+  return input;
+}
+
+
+// function multiplyBy2(num) { return num * 2; }
+// function add7(num) { return num + 7; }
+// function modulo4(num) { return num % 4; }
+// function subtract10(num) { return num - 10; }
+// const arrayOfFunctions = [multiplyBy2, add7, modulo4, subtract10];
+// console.log(flow(2, arrayOfFunctions)); // -> -7
+
+
+//------------------------------------------------------------------------------------------
+
 // Attempt #2 (CSX Solution)
 
 function flow(input, funcArray) {
